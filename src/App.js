81bd5bb9d1cab46ed1d8bb3f8613b47585ae6166
lukaszdutka <lukaszdutka2@gmail.com>
2021-04-24 00:59:06 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -4,16 +4,21 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import { Store } from "./states/App";
 import { theme } from "./themes/theme";
-import { Nav } from "./layout/Nav/Nav";
-import { AppSwitch } from "./switch/Switch";
+import { MeetingView } from "./components/forms/MeetingView";
+import { Container } from "@material-ui/core";
 
 function App() {
     return (
         <Store>
             <ThemeProvider theme={theme}>
                 <Router>
-                    <Nav />
-                    <AppSwitch />
+                    {/*<Nav />*/}
+                    {/*<AppSwitch />*/}
+                    <div className={"black-background"}>
+                        <Container maxWidth="sm">
+                            <MeetingView />
+                        </Container>
+                    </div>
                 </Router>
             </ThemeProvider>
         </Store>
---
+++ src/components/forms/MeetingView.jsx
@@ -0,0 +1,106 @@
+import "../../App.css";
+import {
+    Container,
+    IconButton,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemSecondaryAction,
+    ListItemText,
+    makeStyles,
+    Typography,
+} from "@material-ui/core";
+import SendIcon from "@material-ui/icons/Send";
+import QuestionAnswerIcon from "@material-ui/icons/QuestionAnswer";
+import { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import {FixedTimeProgressBar} from "./FixedTimeProgressBar";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        maxWidth: 752,
+    },
+    demo: {
+        backgroundColor: theme.palette.background.paper,
+    },
+    title: {
+        margin: theme.spacing(4, 0, 2),
+    },
+}));
+
+export const MeetingView = ({ roomId }) => {
+    const classes = useStyles();
+    const [questions, setQuestions] = useState([]);
+
+    useEffect(() => {
+        setQuestions(getQuestionsByRoomId(roomId));
+    }, [roomId]);
+
+    const getQuestionsByRoomId = (roomId) => {
+        console.log("Questions got from server"); //todo api call
+        return [
+            {
+                id: "1",
+                text: "What is love?",
+            },
+            {
+                id: "2",
+                text: "Baby don't hurt me?",
+            },
+            {
+                id: "3",
+                text: "What is REST?",
+            },
+        ];
+    };
+
+    const setQuestionAsAsked = (questionId) => {
+        console.log(`Question is set as asked: ${questionId}`); //todo api call
+        setTimeout(() => {
+            console.log(questions.filter((q) => q.id !== questionId));
+            setQuestions(questions.filter((q) => q.id !== questionId));
+        }, 10000);
+    };
+
+    const onItemClicked = (questionId) => {
+        console.log(`Question asked clicked: ${questionId}`);
+        setQuestionAsAsked(questionId);
+    };
+
+    const listItems = () => {
+        if (!questions || questions.length === 0) {
+            return <ListItem> There are no questions </ListItem>;
+        }
+        return questions.map((question) => {
+            return (
+                <ListItem key={question.id} button onClick={() => onItemClicked(question.id)}>
+                    <ListItemIcon>
+                        <QuestionAnswerIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={`${question.text}`} />
+                    <FixedTimeProgressBar time={10}/>
+                    <ListItemSecondaryAction>
+                        <IconButton edge="end" aria-label="delete" onClick={() => onItemClicked(question.id)}>
+                            <SendIcon />
+                        </IconButton>
+                    </ListItemSecondaryAction>
+                </ListItem>
+            );
+        });
+    };
+    return (
+        <Container maxWidth="sm" style={{ paddingTop: "40%" }}>
+            <Typography variant="h6">Questions</Typography>
+            <div className={classes.demo}>
+                <List>{listItems()}</List>
+            </div>
+        </Container>
+    );
+};
+
+MeetingView.propTypes = {
+    roomId: PropTypes.string,
+    // formError: PropTypes.string,
+    // loading: PropTypes.bool,
+};
