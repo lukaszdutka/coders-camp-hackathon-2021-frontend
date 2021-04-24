cc118323b296f126f886eb922791c1d8828d9747
kacper-cyra <kacper.cyranowski@gmail.com>
2021-04-24 18:21:48 +0200

    Adding table

--- src/components/guestSummary.jsx
+++ src/components/guestSummary.jsx
@@ -1,6 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Grid, List } from "@material-ui/core";
+import { CircularProgress, Grid } from "@material-ui/core";
+import { DataGrid } from "@material-ui/data-grid";
 import { Rooms } from "../api/rooms";
+
+const columns = [
+    { field: "text", headerName: "Question", width: 120 },
+    { field: "correctAnswer", headerName: "Correct answer", width: 150 },
+    { field: "guestAnswer", headerName: "Guest answer", width: 130 },
+    {
+        field: "correct",
+        headerName: "Correct",
+    },
+];
 
 export const GuestSummary = ({ roomId = "6084112b166c3c3e3430215e", email = "test@example.com" }) => {
     const [userSummary, setUserSummary] = useState(null);
@@ -15,20 +26,42 @@ export const GuestSummary = ({ roomId = "6084112b166c3c3e3430215e", email = "tes
         fetchGuestSummary(Rooms.getGuestSummary(roomId, email));
     }, []);
 
-    const getSummary(){
-        const correctAnswers = userSummary.questions.reduce((sum, val) =>  (sum += val.correct), 0);
+    const getSummary = () => {
+        console.log(userSummary);
+        const correctAnswers = userSummary.questions.reduce((sum, val) => (sum += val.correct), 0);
         const incorrectAnswers = userSummary.questions.length - correctAnswers;
 
-        
+        return (
+            <div>
+                {"User email: " + userSummary.email} <br />
+                {"Total questions: " + userSummary.questionsAll} <br />
+                {"Questions answered: " + userSummary.questionsAnswered} <br />
+                {"Correct answers: " + correctAnswers} <br />
+                {"Incorrect answers: " + incorrectAnswers}
+            </div>
+        );
+    };
+
+    function mapRows() {
+        const result = userSummary.questions.map(({ correct, correctAnswer, guestAnswer, text }, index) => {
+            return { correct, correctAnswer, guestAnswer, text, id: index };
+        });
+        console.log(result);
+        return result;
     }
 
     return (
         <Grid item xs={12}>
-            <div>
-                {userSummary
-                    ? "Correct answers: " + 
-                    : "Nothing"}
-            </div>
+            {userSummary ? (
+                <>
+                    {/* <div> {getSummary()} </div> */}
+                    <div style={{ height: 400 }}>
+                        <DataGrid rows={mapRows()} columns={columns} pageSize={5} checkboxSelection />
+                    </div>
+                </>
+            ) : (
+                <CircularProgress />
+            )}
         </Grid>
     );
 };
