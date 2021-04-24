e748af3c4672981484ed605c274ad7e4d2593220
lukaszdutka <lukaszdutka2@gmail.com>
2021-04-24 17:03:36 +0200

    add some styling;

--- src/components/lists/QuestionsList.jsx
+++ src/components/lists/QuestionsList.jsx
@@ -3,12 +3,22 @@ import { Grid, Typography, List, IconButton } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 
 import { QuestionEdit } from "../forms/QuestionEdit";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+    smallPadding: {
+        padding: "20px",
+    },
+}));
 
 export const QuestionsList = ({ questionsArray, updateQuestion, addQuestion }) => {
+    const classes = useStyles();
     return (
         <Grid item xs={12}>
             <List>
-                <Typography variant="h6">Collection</Typography>
+                <Typography className={classes.smallPadding} variant="h5">
+                    Collection
+                </Typography>
                 {!!questionsArray.length
                     ? questionsArray.map((question) => {
                           return (
