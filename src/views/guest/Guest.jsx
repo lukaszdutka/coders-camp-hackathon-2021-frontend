9dde44b2d30f3ef3f12ad72b6b06122c44a970d2
Aleksandra Zochowska <a.j.zochowska@gmail.com>
2021-04-24 17:53:33 +0200

    styled guest introduction form

--- src/views/guest/Guest.jsx
+++ src/views/guest/Guest.jsx
@@ -4,8 +4,42 @@ import { Rooms } from "../../api/rooms";
 import { GuestForm } from "../../components/forms/GuestForm";
 import { Typography, Paper } from "@material-ui/core";
 import { Question } from "../../components/forms/Question";
+import { makeStyles } from "@material-ui/core/styles";
+
+
+
+const useStyles = makeStyles((theme) => ({
+    guestBeginningFormContainer: {
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    guestBeginningForm: {
+        padding: "3rem",
+        flexBasis: "40%",
+        // minidth: "1000px",
+    },
+    // loginImg: {
+    //     border: "6px solid #91C43B",
+    //     borderRadius: "10%",
+    //     marginTop: "25px",
+    // },
+    // action: {
+    //     marginTop: "30px",
+    // },
+    // paddingBot: {
+    //     marginTop: "80px",
+    //     paddingBottom: "30px",
+    // },
+    // paddingBotBig: {
+    //     marginTop: "40px",
+    //     marginBottom: "60px",
+    // },
+}));
 
 export const Guest = () => {
+    const classes = useStyles();
     const { id } = useParams();
     const [room, setRoom] = useState(null);
     const [guestName, setGuestName] = useState("");
@@ -25,23 +59,25 @@ export const Guest = () => {
         }
     }, [guestEmail, id]);
     return (
-        <div>
-            {!guestName && !guestEmail ? (
-                <GuestForm roomId={id} setGuestEmail={setGuestEmail} setGuestName={setGuestName} />
-            ) : (
-                <Paper>
-                    <Typography variant="h5">Welcome {guestName}</Typography>
-                    <Typography variant="body1">Please wait for questions</Typography>
-                </Paper>
-            )}
-            {activeQuestion.activeQuestion && guestName && guestEmail && (
-                <Question
-                    question={activeQuestion}
-                    setActiveQuestion={setActiveQuestion}
-                    roomId={id}
-                    email={guestEmail}
-                />
-            )}
+        <div className={classes.guestBeginningFormContainer}>
+            <div className={classes.guestBeginningForm}>
+                {!guestName && !guestEmail ? (
+                    <GuestForm roomId={id} setGuestEmail={setGuestEmail} setGuestName={setGuestName} />
+                ) : (
+                    <Paper>
+                        <Typography variant="h5">Welcome {guestName}</Typography>
+                        <Typography variant="body1">Please wait for questions</Typography>
+                    </Paper>
+                )}
+                {activeQuestion.activeQuestion && guestName && guestEmail && (
+                    <Question
+                        question={activeQuestion}
+                        setActiveQuestion={setActiveQuestion}
+                        roomId={id}
+                        email={guestEmail}
+                    />
+                )}
+            </div>
         </div>
     );
 };
